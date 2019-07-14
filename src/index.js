d41eb5cc2fb8d91a47016e120c63a410caec6892
Ben <mail1@sonadresse.com>
2019-07-14 17:09:19 +0000

    sync

--- src/index.js
+++ src/index.js
@@ -10,31 +10,25 @@
 /*******************************************
   GLOBALS
 *********************************************/
-const {
-      BaseKonnector ,
-      requestFactory,
-      log           ,
-    }                  = require('cozy-konnector-libs'),
-      authenticate     = require('./authentication'   ),
-      retrieveInvoices = require('./retrieveInvoices' ),
-      retriveArticles  = require('./retriveArticles'  )
+const { BaseKonnector, requestFactory, log } = require('cozy-konnector-libs'),
+  authenticate = require('./authentication'),
+  retrieveInvoices = require('./retrieveInvoices'),
+  retriveArticles = require('./retriveArticles')
 
-var   CTXT          // connector contextual variables, inited later on
-var   FOLDER_PATH   // retrieved in "fields"
-var   FIELDS
-
+var CTXT // connector contextual variables, inited later on
+var FOLDER_PATH // retrieved in "fields"
+var FIELDS
 
 /*******************************************
   COMMAND LINE PARAMETERS
   expected parameters
     --debug
 *********************************************/
-process.argv.forEach(function (val, index, array) {
+process.argv.forEach(function(val, index, array) {
   if (val === '--debug') {
     CTXT.DEBUG_MODE = true
   }
 })
-
 
 /*******************************************
   MAIN
@@ -42,52 +36,50 @@ process.argv.forEach(function (val, index, array) {
 const baseKonnector = new BaseKonnector(start)
 
 async function start(fields) {
-
   initCTXT(fields, this)
 
   log('info', 'Authenticating ...')
-  // await authenticate(CTXT)
+  await authenticate(CTXT)
   log('info', 'Successfully logged in')
 
-  log('info', 'Fetching the invoices ...')
+  // log('info', 'Fetching the invoices ...')
   // await retrieveInvoices(CTXT)
 
   log('info', 'Fetching the articles')
-  // await retriveArticles(CTXT)
-
+  await retriveArticles(CTXT)
 }
 
 module.exports = baseKonnector
-
 
 /*******************************************
   INIT CTXT VARIABLES
 *********************************************/
 function initCTXT(fields, baseKonnector) {
-  const req  =  requestFactory({cheerio:false,json:false})
+  const req = requestFactory({ cheerio: false, json: false })
   CTXT = {
-            bookmarksList : []       , // retrieved bookmarks list
-            baseKonnector : baseKonnector ,
-            authorization : undefined, // retrieved during API authentication
-            formKey       : undefined, // retrieved during account authentication
-            cookieJar     : req.jar(), // the cookies for all the requests
-            fields        : fields   , // fields retrieved from the user interface (login, pwd...)
-            request       : req      ,
-            DEBUG_MODE    : false    ,
-            requestFactory: requestFactory ,
-            history       : baseKonnector.getAccountData(),
-            USER_AGENT    : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.108 Safari/537.36',
-            // add           : add
-          }
+    bookmarksList: [], // retrieved bookmarks list
+    baseKonnector: baseKonnector,
+    authorization: undefined, // retrieved during API authentication
+    formKey: undefined, // retrieved during account authentication
+    cookieJar: req.jar(), // the cookies for all the requests
+    fields: fields, // fields retrieved from the user interface (login, pwd...)
+    request: req,
+    DEBUG_MODE: false,
+    requestFactory: requestFactory,
+    history: baseKonnector.getAccountData(),
+    USER_AGENT:
+      'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.108 Safari/537.36'
+    // add           : add
+  }
 
-  if(!CTXT.history) CTXT.history = []
-  console.log("ACCOUNT DATA retrieved :", CTXT.history );
-  console.log(typeof CTXT.history )
-  console.log({a:1} )
+  CTXT.history = [] // TODO : remove
+  if (!CTXT.history) CTXT.history = []
+  console.log('ACCOUNT DATA retrieved :', CTXT.history)
+  console.log(typeof CTXT.history)
+  console.log({ a: 1 })
   console.log(baseKonnector.getAccountData())
 
-
-/*******************************************
+  /*******************************************
   HISTORY OF DOWNLOADED ARTICLES MANAGEMENT
   CTXT.history.add(item)
   item = {
@@ -95,15 +87,11 @@ function initCTXT(fields, baseKonnector) {
     lesechosId : string
   }
 *********************************************/
-  CTXT.history.add = function (item) {
+  CTXT.history.add = function(item) {
     this.push(item)
-    baseKonnector.saveAccountData(CTXT.history.slice(), {merge:false}) // send a full array each time ?
+    baseKonnector.saveAccountData(CTXT.history.slice(), { merge: false }) // send a full array each time ?
   }
 }
-
-
-
-
 
 //
 //
--- src/retriveArticles.js
+++ src/retriveArticles.js
@@ -1,93 +1,102 @@
 /*******************************************
   GLOBALS
 *********************************************/
-const {
-      log           ,
-      errors        ,
-      cozyClient    ,
-    }                  = require('cozy-konnector-libs'),
-      Promise          = require('bluebird'           ),
-      sanitizeFileName = require('sanitize-filename'  ),
-      moment           = require('moment'             ),
-      parseArticle     = require('./article-parser'   )
-
+const { log, errors, cozyClient } = require('cozy-konnector-libs'),
+  Promise = require('bluebird'),
+  sanitizeFileName = require('sanitize-filename'),
+  moment = require('moment'),
+  parseArticle = require('./article-parser')
 
 var CTXT // the context sent by index.js
-module.exports = (ctxt) => {
+module.exports = ctxt => {
   CTXT = ctxt
-  return Promise.resolve(true)
   return retriveArticles()
+  // return Promise.resolve(true)
 }
-
-
 
 /*******************************************
   BOOKMARKS RETRIEVAL
 ********************************************/
 function retriveArticles() {
-  return retrieveBookmarksList(1)
-  .then( ()=>{
-    log('info', 'all bookmarks list downloaded, there are ' + CTXT.bookmarksList.length + ' articles')
+  return retrieveBookmarksList(1).then(() => {
+    log(
+      'info',
+      'all bookmarks list downloaded, there are ' +
+        CTXT.bookmarksList.length +
+        ' articles'
+    )
     return retrieveArticles()
   })
 }
 
 function retrieveBookmarksList(page) {
-  console.log("totototot");
-  return CTXT.requestFactory({jar:true,cheerio:false,json:true})
-  .get({
-    url : 'https://api.lesechos.fr/api/v1/auth/me/bookmarks',
-    qs  : { page: page },
-    headers: {
-      Authorization  : CTXT.authorization,
-      Origin         : 'https://www.lesechos.fr',
-      Accept         : 'application/json, text/plain, */*',
-      'cache-control': 'no-cache',
-      'User-Agent'   : CTXT.USER_AGENT,
-    }
-  })
-  .then(data => {
-    CTXT.bookmarksList = CTXT.bookmarksList.concat(data.items)
-    if (data.currentPage < data.totalPages) {
-      return retrieveBookmarksList(page+1)
-    }else {
-      console.log('fin récursion');
-      prepareBookmarksFileNames()
-      return filterAlreadyDownladedArticles()
-    }
-  })
-  .catch( error => {
-    log('error', 'during bookmark list retrieval')
-    log('error', error)
-    throw new Error(errors.VENDOR_DOWN)
-  })
+  console.log('totototot', page)
+  return CTXT.requestFactory({ jar: true, cheerio: false, json: true })
+    .get({
+      url: 'https://api.lesechos.fr/api/v1/auth/me/bookmarks',
+      qs: { page: page },
+      headers: {
+        Authorization: CTXT.authorization,
+        Origin: 'https://www.lesechos.fr',
+        Accept: 'application/json, text/plain, */*',
+        'cache-control': 'no-cache',
+        'User-Agent': CTXT.USER_AGENT
+      }
+    })
+    .then(data => {
+      CTXT.bookmarksList = CTXT.bookmarksList.concat(data.items)
+      if (data.currentPage < data.totalPages) {
+        return retrieveBookmarksList(page + 1)
+      } else {
+        console.log('fin récursion')
+        prepareBookmarksFileNames()
+        // return filterAlreadyDownladedArticles() TODO
+      }
+    })
+    .catch(error => {
+      log('error', 'during bookmark list retrieval')
+      log('error', error)
+      throw new Error(errors.VENDOR_DOWN)
+    })
 }
 
 function prepareBookmarksFileNames() {
-  console.log('prepareBookmarksFileNames()');
-  CTXT.bookmarksList.forEach( bookmark => {
-    bookmark.filename = sanitizeFileName( moment(bookmark.publicationDate).format('YYYY-MM-DD') + ' - ' + bookmark.title) + '.html'
+  console.log('prepareBookmarksFileNames()')
+  CTXT.bookmarksList.forEach(bookmark => {
+    bookmark.filename =
+      sanitizeFileName(
+        moment(bookmark.publicationDate).format('YYYY-MM-DD') +
+          ' - ' +
+          bookmark.title
+      ) + '.html'
   })
 }
-
 
 /*******************************************
   ARTICLES RETRIEVAL
 ********************************************/
+
 function retrieveArticles() {
-  return Promise.map(CTXT.bookmarksList, bookmark=>{
-    return isAlreadyDownloaded(bookmark)
-    .then(bookmark => retrieveArticle(bookmark))  // TODO juste retrieveArticle ?
-    .then(article  => saveArticle(article))
-    .catch(error   => {
-      if (error.message === 'fileAlreadyHere') {
-        log('info', `useless to download https://www.lesechos.fr/x/x/-${bookmark.id}`)
-      }else{
-        log('error', 'while retrieving or saving article')
-        log('error', error.message) // TODO throw an error ?
-      }
-    })
-  }, {concurrency:5})
+  return Promise.map(
+    CTXT.bookmarksList,
+    bookmark => {
+      return isAlreadyDownloaded(bookmark)
+        .then(bookmark => retrieveArticle(bookmark)) // TODO juste retrieveArticle ?
+        .then(article => saveArticle(article))
+        .catch(error => {
+          if (error.message === 'fileAlreadyHere') {
+            log(
+              'info',
+              `useless to download https://www.lesechos.fr/x/x/-${bookmark.id}`
+            )
+          } else {
+            log('error', 'while retrieving or saving article')
+            log('error', error.message) // TODO throw an error ?
+          }
+        })
+    },
+    { concurrency: 5 }
+  )
 }
 
 async function isAlreadyDownloaded(bookmark) {
@@ -100,46 +109,56 @@ async function isAlreadyDownloaded(bookmark) {
 }
 
 function retrieveArticle(bookmark) {
-  log('info', 'fetch article from ' + `https://www.lesechos.fr/x/x/-${bookmark.id}`)
-  return CTXT.requestFactory({jar:CTXT.cookieJar,cheerio:true,json:false})
-  .get({
-    url    : `https://www.lesechos.fr/x/x/-${bookmark.id}`,
-    jar    : CTXT.cookieJar,
-    headers: {
-      Accept                     : 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3'
-      'cache-control'            : 'no-cache',
-      'User-Agent'               : CTXT.USER_AGENT,
-      'Upgrade-Insecure-Requests': '1',
-    }
+  log(
+    'info',
+    'fetch article from ' + `https://www.lesechos.fr/x/x/-${bookmark.id}`
+  )
+  return CTXT.requestFactory({
+    jar: CTXT.cookieJar,
+    cheerio: true,
+    json: false
   })
-  .then(html$ => {
-    let article = {
-      rawHtml    : html$.html(),
-      html$      : html$,
-      filename   : bookmark.filename,
-      url        : `https://www.lesechos.fr/x/x/-${bookmark.id}`,
-      baseUrl    : 'https://www.lesechos.fr',
-      lesechosId : bookmark.id,
-    }
-    return parseArticle(article)
-  })
+    .get({
+      url: `https://www.lesechos.fr/x/x/-${bookmark.id}`,
+      jar: CTXT.cookieJar,
+      headers: {
+        Accept:
+          'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
+        'cache-control': 'no-cache',
+        'User-Agent': CTXT.USER_AGENT,
+        'Upgrade-Insecure-Requests': '1'
+      }
+    })
+    .then(html$ => {
+      let article = {
+        rawHtml: html$.html(),
+        html$: html$,
+        filename: bookmark.filename,
+        url: `https://www.lesechos.fr/x/x/-${bookmark.id}`,
+        baseUrl: 'https://www.lesechos.fr',
+        lesechosId: bookmark.id
+      }
+      return parseArticle(article)
+    })
 }
 
 async function saveArticle(article) {
+  const fs = require('fs')
+  fs.writeFileSync(article.filename, article.inlinedHtml)
   const filename = article.filename
   // test existance of a file at this path (should not happen)
-  const fileDoc = await cozyClient.files.statByPath(CTXT.fields.path + filename)
+  // const fileDoc = await cozyClient.files.statByPath(CTXT.fields.path + filename)
   if (fileDoc) throw new Error('fileAlreadyHere')
   // get dirID and save
-  const dirDoc  = await cozyClient.files.statByPath(CTXT.fields.path) // todo : est on sûr que dirDoc existe ?
-  const fileDoc = await cozyClient.files.create(article.inlinedHtml, {
-    name        : filename    ,
-    dirID       : dirDoc._id  ,  // TODO CTXT.fields.path   cozyClient.files.statByPath(path) ._id
-    contentType : 'text/html' ,
+  // const dirDoc  = await cozyClient.files.statByPath(CTXT.fields.path) // todo : est on sûr que dirDoc existe ?
+  fileDoc = await cozyClient.files.create(article.inlinedHtml, {
+    name: filename,
+    dirID: dirDoc._id, // TODO CTXT.fields.path   cozyClient.files.statByPath(path) ._id
+    contentType: 'text/html'
   })
   log('info', 'save file ' + article.filename)
   return CTXT.history.add({
-    lesechosId : article.lesechosId,
-    cozyId     : fileDoc._id,
+    lesechosId: article.lesechosId,
+    cozyId: fileDoc._id
   })
 }
